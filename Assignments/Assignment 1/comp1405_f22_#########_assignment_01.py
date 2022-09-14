eea7e751ba65adac645ccaa8d344e1314ca2b23f
FlyingEwok <nicholas.garth1@gmail.com>
2022-09-14 12:00:13 -0400

    Set up pygame window

---
+++ Assignments/Assignment 1/comp1405_f22_#########_assignment_01.py
@@ -0,0 +1,29 @@
+from cmath import rect
+import pygame
+from sympy import true
+
+def createPygameWindow(x: int, y: int) -> rect:
+    """Takes in dimensions and creates a pygame window"""
+    # Intialize pygame
+    pygame.display.init()
+    # Create Window
+    window = pygame.display.set_mode(size=(x,y))
+    return window
+def drawMyAssignedImage(surface):
+    """Draws a image to the pygame window"""
+    rectColour = (82,144,60) # Green
+    pygame.draw.rect(surface, rectColour, pygame.Rect(30, 30, 60, 60))
+    pygame.display.flip()
+
+# Main
+
+# Draw Image
+window = createPygameWindow(480,640) # Make a 480 by 640 window
+# Create image while keeping the window open and close if the X button is clicked
+while true: # Loop through infinently
+    # Constantly check if x button is clicked
+    for event in pygame.event.get(): 
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit(0) # Removes text dumb to the shell
+        drawMyAssignedImage(window)
