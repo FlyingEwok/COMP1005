47b963083b2bf5b6b78ec895207185467b9d10ab
FlyingEwok <nicholas.garth1@gmail.com>
2022-09-14 18:55:55 -0400

    Fixed typo & removed hard-coded file name

--- Assignments/Assignment 1/comp1405_f22_101227727_assignment_01.py
+++ Assignments/Assignment 1/comp1405_f22_101227727_assignment_01.py
@@ -12,7 +12,7 @@ def createPygameWindow(x: int, y: int) -> pygame.surface:
 
 def drawMyAssignedImage(surface: pygame.surface) -> None:
     """Draws a image to the pygame window"""
-    backGroundColour = (255,255,255) # White RGB 
+    backgroundColour = (255,255,255) # White RGB 
     rectColour = (82,144,60) # Green RGB
     polygonColour = (131, 117, 175) # Purple RGB
 
@@ -20,18 +20,18 @@ def drawMyAssignedImage(surface: pygame.surface) -> None:
     polygonVerticies = [(320,106), (240,106), (321,0), (400,106), (400, 213)]
 
     # Draw!
-    surface.fill(backGroundColour) # Set background colour to white
+    surface.fill(backgroundColour) # Set background colour to white
     pygame.draw.rect(surface, rectColour, pygame.Rect(320, 106, 81, 321)) # Draw Rectangle
     pygame.draw.polygon(surface, polygonColour, polygonVerticies) # Draw the polygon
     pygame.display.flip() # Update the display so the drawn images show
 
-def saveFrameAsImage(surface: pygame.surface) -> None:
+def saveFrameAsImage(surface: pygame.surface, fileName) -> None:
     """When called, the current frame will be saved"""
-    fileName = 'assigned_image_for_101227727.png'
     pygame.image.save(surface, fileName)
 
 # Main
 
+imageFileName = 'assigned_image_for_101227727.png' # Set variable as string for image file name
 # Draw Image
 window = createPygameWindow(480,640) # Make a 480 by 640 window
 # Create image while keeping the window open and close if the X button is clicked
@@ -39,7 +39,7 @@ while True: # Loop through infinently
     # Constantly check if x button is clicked
     for event in pygame.event.get(): 
         if event.type == pygame.QUIT:
-            saveFrameAsImage(window) # Save the last frame as image (In this case only frame)
+            saveFrameAsImage(window, imageFileName) # Save the last frame as image (In this case only frame)
             pygame.quit()
             exit(0) # Removes text dump from the shell
         drawMyAssignedImage(window)
