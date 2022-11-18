c22af36b7f00ae55b1f73dc6d7c32797b6817339
FlyingEwok <nicholas.garth1@gmail.com>
2022-11-17 20:22:52 -0500

    Test shit

---
+++ Tests/Test 2/test_1.py
@@ -0,0 +1,23 @@
+def shm(qux, gzk):
+    qux += gzk
+
+def xyz(w):
+    a = 0
+    a += w
+    return a
+    
+def ham(i, bar):
+    bar += i
+
+aap = 92
+ukr = 1
+bar = 27
+egg = 89
+foo = 45
+gzk = 67
+qux = 12
+shm(qux,gzk)
+foo = xyz(foo)
+ham(87, bar)
+
+
